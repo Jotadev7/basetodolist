8a2282473d4ae465781f385b9b30334b6fb11b59
Joao <jotadev7@gmail.com>
2022-02-01 11:21:37 -0300

    Criado componente de to-do

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <h1>
-    Welcome to {{title}}.
-  </h1>
-  <app-input-button-unit></app-input-button-unit>
-  <app-input-button-unit></app-input-button-unit>
-  <app-input-button-unit></app-input-button-unit>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'basetodolist';
+  todoList = [
+    {title: 'Tarefa 1'},
+    {title: 'Tarefa 2'},
+    {title: 'Tarefa 3'},
+    {title: 'Tarefa 4'},
+    {title: 'Tarefa 5'},
+    {title: 'Tarefa 6'},
+  ]
 }
