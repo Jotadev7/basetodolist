f7606744a09594deb8266325163cfeb25821bc2d
Joao <jotadev7@gmail.com>
2022-02-02 13:32:29 -0300

    Ligando formulário a objeto

--- src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,7 +9,9 @@ export class CadastroComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    let nome = new FormControl('');
+    
   }
 
 }
