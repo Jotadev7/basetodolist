30c1dfc2a9ba701a866feb17d4078635305507ff
Joao <jotadev7@gmail.com>
2022-02-07 16:09:58 -0300

    Projeto consumindo fake back-end, CRUD finalizado

--- src/app/teste/carro/carro.component.ts
+++ src/app/teste/carro/carro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Car } from 'src/app/models/car';
+import { CarService } from 'src/app/services/car.service';
 
 @Component({
   selector: 'app-carro',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarroComponent implements OnInit {
 
-  constructor() { }
+  car = {} as Car;
+  cars: Car[] = [];
+
+  constructor(private carService: CarService) { }
 
   ngOnInit(): void {
+    this.getCars();
+  }
+
+  // Define se um carro será criado ou atualizado
+  saveCar(form: NgForm) {
+    if (this.car.id !== undefined) {
+      this.carService.updateCar(this.car).subscribe(() => {
+        this.cleanForm(form);
+      });
+    } else {
+      this.carService.saveCar(this.car).subscribe(() => {
+        this.cleanForm(form);
+      });
+    }
+  }
+
+  // Chama o serviço para obter todos os carros
+  getCars() {
+    this.carService.getCars().subscribe((cars: Car[]) => {
+      this.cars = cars;
+    });
+  }
+
+  // Deleta um carro
+  deleteCar(car: Car) {
+    this.carService.deleteCar(car).subscribe(() => {
+      this.getCars();
+    });
+  }
+
+  // Copia o carro para ser editado
+  editCar(car: Car) {
+    this.car = { ...car };
+  }
+
+  // limpa o formulario
+  cleanForm(form: NgForm) {
+    this.getCars();
+    form.resetForm();
+    this.car = {} as Car;
   }
 
 }
